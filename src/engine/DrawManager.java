a6f087871a3081b2174a7c0df61557b360310e25
GeonYeop Na <ngy2330@gmail.com>
2024-11-15 21:41:27 +0900

    FEAT: Add Turtle Boss(BossB) Graphic; The Boss has 30x36 size and 3 each sprite.

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -124,7 +124,11 @@ public class DrawManager {
 		BossARight1,
 		BossALeft2,
 		BossAMiddle2,
-		BossARight2
+		BossARight2,
+
+		BossB1,
+		BossB2,
+		BossB3
 
 	};
 
@@ -178,6 +182,9 @@ public class DrawManager {
 			spriteMap.put(SpriteType.BossALeft2, new boolean[12][24]);
 			spriteMap.put(SpriteType.BossAMiddle2, new boolean[12][24]);
 			spriteMap.put(SpriteType.BossARight2, new boolean[12][24]);
+			spriteMap.put(SpriteType.BossB1, new boolean[30][36]);
+			spriteMap.put(SpriteType.BossB2, new boolean[30][36]);
+			spriteMap.put(SpriteType.BossB3, new boolean[30][36]);
 
 			fileManager.loadSprite(spriteMap);
 			logger.info("Finished loading the sprites.");
